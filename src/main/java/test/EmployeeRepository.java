ec7b853acb89916bf4566f28f83abac52450c394
Dennis Crissman <dcrissma@redhat.com>
2016-07-08 09:41:01 -0400

    initial

---
+++ src/main/java/test/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+    Employee findByFirstName(String firstName);
+
+    List<Employee> findByLastName(String lastName);
+
+}
